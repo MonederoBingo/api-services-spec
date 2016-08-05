0d5176159332dc6558559dcf68f27fb0906feffa
Alonso Ortega <Alonso.Ortega.ctr@sabre.com>
2016-08-04 20:31:21 -0500

    Adding message verifification in feature.

--- src/features/company_registration.feature
+++ src/features/company_registration.feature
@@ -2,4 +2,7 @@ Feature: Registering new company
   Scenario: Non existent company user provides correct information
     Given User provides correct registration information
     When User sends registration request
-    Then The user should receive a message reporting that was successfully added
+    Then The response should be successful
+    And The user should receive a message the following messages
+      | ENGLISH | We've sent you an email. Open it up to activate your account. If you do not receive that email within 1 hour, please email support@monederobingo.com |
+      | SPANISH | Se ha enviado un link de activación a su correo. Si no lo recibe dentro de una hora, favor de enviar un correo a support@monederobingo.com |
--- src/test/java/com/monederobingo/step_definitions/CompanyRegistration.java
+++ src/test/java/com/monederobingo/step_definitions/CompanyRegistration.java
@@ -1,30 +1,47 @@
 /* Copyright 2016 Sabre Holdings */
 package com.monederobingo.step_definitions;
 
-import com.monederobingo.tests.use_cases.auth.registration.company.SuccessfulRegistration;
+import com.monederobingo.api.client.model.Language;
+import com.monederobingo.api.client.model.ServiceResult;
+import com.monederobingo.api.client.requests.auth.registration.CompanyRegistrationRequest;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class CompanyRegistration
 {
-    SuccessfulRegistration successfulRegistration = new SuccessfulRegistration();
+    private CompanyRegistrationRequest companyRegistrationRequest;
+    private ServiceResult serviceResult;
 
     @Given("^User provides correct registration information$")
-    public void test()
+    public void userProvidesCorrectRegistrationInformation()
     {
-
+        companyRegistrationRequest = new CompanyRegistrationRequest();
     }
 
     @When("^User sends registration request$")
     public void userSendRegistrationRequest() throws Throwable
     {
-        successfulRegistration.sendRegistrationRequest();
+        serviceResult = companyRegistrationRequest.send();
     }
 
-    @Then("^The user should receive a message reporting that was successfully added$")
-    public void theUserShouldReceiveAMessageReportingThatWasSuccessfullyAdded() throws Throwable
+    @Then("^The response should be successful$")
+    public void theResponseShouldBeSuccessful() throws Throwable
     {
-       successfulRegistration.asserts();
+        assertTrue(serviceResult.isSuccess());
+    }
+
+    @And("^The user should receive a message the following messages$")
+    public void theUserShouldReceiveAMessageTheFollowingMessages(Map<Language, String> expectedMessages) throws Throwable
+    {
+        assertEquals(expectedMessages.get(Language.ENGLISH), serviceResult.getMessage());
+        assertEquals(expectedMessages.get(Language.ENGLISH), serviceResult.getTranslation(Language.ENGLISH));
+        assertEquals(expectedMessages.get(Language.SPANISH), serviceResult.getTranslation(Language.SPANISH));
     }
 }
