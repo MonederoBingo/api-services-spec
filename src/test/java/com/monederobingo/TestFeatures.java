b66eb4cc9a3f8c963e055f178659711b741ffd3a
alayor <alayor3@gmail.com>
2017-06-25 21:26:24 -0500

    Add discovery service to startup script.

--- src/test/java/com/monederobingo/TestFeatures.java
+++ src/test/java/com/monederobingo/TestFeatures.java
@@ -1,14 +1,15 @@
 package com.monederobingo;
 
+import static cucumber.api.SnippetType.CAMELCASE;
+
 import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = { "junit:output" },
-        snippets = SnippetType.CAMELCASE,
+        snippets = CAMELCASE,
         glue = { "com.monederobingo.step_definitions" },
         features = { "src/features/" },
         monochrome = true)
